83d4e41e9bd210de1b501fcf8d4d496056e02eab
Elizabeth Wandia Mugo <wwandia173@gmail.com>
2022-10-02 14:20:42 +0300

    Done

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,14 +1,15 @@
+
 class DirectorsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
     directors = Director.all
-    render json: directors
+    render json: directors, include: ['movies', 'movies.reviews']
   end
 
   def show
     director = Director.find(params[:id])
-    render json: director
+    render json: director, include: ['movies', 'movies.reviews']
   end
 
   private
---
+++ app/serializers/director_movie_serializer.rb
@@ -0,0 +1,7 @@
+class DirectorMovieSerializer < ActiveModel::Serializer
+
+  attributes :title, :year
+
+  has_many :reviews
+
+end
